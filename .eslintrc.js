c44e16e5684363d035657b80dae77cf785e977ec
Eric Tsai <eric.tsai@tenmax.io>
2017-08-18 22:51:03 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "standard",
+    "plugin:react/all"
+  ],
+  "plugins": [
+    "react"
+  ],
+  "settings": {
+    "react": {
+      "pragma": "React"
+    }
+  }
+}
