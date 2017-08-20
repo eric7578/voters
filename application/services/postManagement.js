421fa2fb70c57138b0f12bc62d7115a9e8e3e3c2
ericyan <eric7578@gmail.com>
2017-08-20 11:40:21 +0800

    add findInsertPosition to find the correct place for post

--- application/services/postManagement.js
+++ application/services/postManagement.js
@@ -9,6 +9,23 @@ function createPost (title) {
   }
 }
 
+function findInsertPosition (post, posts) {
+  let start = 0
+  let end = posts.length - 1
+  const { numUpvote } = post
+
+  // if the first post has the smaller/same numUpvote, place the new post it in front all posts
+  if (posts[start].numUpvote <= numUpvote) {
+    return start
+  } else if (posts[end].numUpvote > numUpvote) {
+    // if the last post has larger numUpvote, place the new post it in the end
+    return posts.length
+  } else {
+    return posts.findIndex(post => post.numUpvote === numUpvote)
+  }
+}
+
 module.exports = {
-  createPost
+  createPost,
+  findInsertPosition
 }
--- application/services/postManagement.test.js
+++ application/services/postManagement.test.js
@@ -10,3 +10,48 @@ test('#createPost should init numUpvote/numDownvote as 0, and generate id', t =>
   t.is(post.numUpvote, 0)
   t.is(post.numDownvote, 0)
 })
+
+test('#insertPost should insert the post according to the numUpvote decedent', t => {
+  const posts = [
+    { numUpvote: 50 },
+    { numUpvote: 40 },
+    { numUpvote: 30 },
+    { numUpvote: 20 },
+    { numUpvote: 10 }
+  ]
+  const post = { numUpvote: 30 }
+
+  const index = postManagement.findInsertPosition(post, posts)
+
+  t.is(index, 2)
+})
+
+test('#insertPost should insert the post with most numUpvote in front', t => {
+  const posts = [
+    { numUpvote: 50 },
+    { numUpvote: 40 },
+    { numUpvote: 30 },
+    { numUpvote: 20 },
+    { numUpvote: 10 }
+  ]
+  const post = { numUpvote: 60 }
+
+  const index = postManagement.findInsertPosition(post, posts)
+
+  t.is(index, 0)
+})
+
+test('#insertPost should insert the post with least numUpvote in front', t => {
+  const posts = [
+    { numUpvote: 50 },
+    { numUpvote: 40 },
+    { numUpvote: 30 },
+    { numUpvote: 20 },
+    { numUpvote: 10 }
+  ]
+  const post = { numUpvote: 5 }
+
+  const index = postManagement.findInsertPosition(post, posts)
+
+  t.is(index, posts.length)
+})
